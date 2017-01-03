220f37b7c91384e290140adfa66f561d08984239
bbazsy <bbozsogi@MacBook-2.local>
2017-01-03 22:17:20 +0100

    first working

--- week1/traffic_light_basic/traffic_light_basic.ino
+++ week1/traffic_light_basic/traffic_light_basic.ino
@@ -1,9 +1,30 @@
+#define RED 4
+#define YELLOW 8
+#define GREEN 12
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(RED, HIGH);
+  delay(1000);
+  digitalWrite(YELLOW, HIGH);
+  delay(1000);
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, HIGH);
+  delay(2000);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, HIGH);
+  delay(1000);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(RED, HIGH);
+  delay(1000);
+  
 }
